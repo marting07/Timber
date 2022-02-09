f2687d9a8b7a50d29699ee95fbf06ffe1e700b82
Martin Ruiz <jose.martin07@gmail.com>
2022-02-08 18:06:45 -0600

    Added the main loop of the game and registered the exit key, we also applied the double buffering basics

--- Timber.cpp
+++ Timber.cpp
@@ -11,6 +11,33 @@ int main()
     VideoMode vm(1024, 768);
     // Create and open a window for the game
     RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+    while (window.isOpen())
+    {
+        /*
+        ****************************************
+        Handle the players input
+        ****************************************
+        */
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        /*
+        ****************************************
+        Update the scene
+        ****************************************
+        */
+        /*
+        ****************************************
+        Draw the scene
+        ****************************************
+        */
+        // Clear everything from the last frame
+        window.clear();
+        // Draw our game scene here
+        // Show everything we just drew
+        window.display();
+    }
     return 0;
 }
 
